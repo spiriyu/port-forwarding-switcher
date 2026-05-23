caf9141937ad6b94fe9eec3e38d863b2560fe77d
Yaron Atid <yaronatidim@gmail.com>
2026-05-23 23:52:13 +0000

    chore: update DEFAULT_DAEMON_PORT to 65432

---
+++ libs/shared/src/config/defaults.ts
@@ -0,0 +1,16 @@
+import { PortswitchConfig } from '../types/config';
+
+export const DEFAULT_DAEMON_PORT = 65432;
+export const CURRENT_SCHEMA_VERSION = 1;
+
+export const DEFAULT_CONFIG: PortswitchConfig = {
+  schemaVersion: CURRENT_SCHEMA_VERSION,
+  daemon: {
+    port: DEFAULT_DAEMON_PORT,
+    logRetention: {
+      maxFiles: 10,
+      maxFileBytes: 5 * 1024 * 1024,
+    },
+  },
+  mappings: [],
+};
